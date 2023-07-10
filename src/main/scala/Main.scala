32a07f35747a568b73a384950399da492a2ed825
Titouan Cocheril <titouan.cocheril25@gmail.com>
2023-07-10 23:47:17 +0200

    change algo to in-depth search

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,4 +1,4 @@
-import play.api.libs.json.{JsObject, Json}
+import play.api.libs.json._
 
 object Main {
   case class CurrencyData(costPred: Float, namePred: Option[String])
@@ -23,64 +23,58 @@ object Main {
     }.toMap
 
     currencyRate ++= newCurrencyRate
-    currencyRate("EUR") = currencyRate("EUR").copy(costPred = 1.0f)
+    currencyRate("BTC") = currencyRate("BTC").copy(costPred = 0.0f)
   }
 
   private def release(firstCurrency: String, secondCurrency: String): CurrencyData = {
     val newCostPred = currencyRate(firstCurrency).costPred + ratesMap(s"$firstCurrency-$secondCurrency")
 
-    println(newCostPred)
-    println(currencyRate(secondCurrency))
-
-    if (currencyRate(secondCurrency).costPred > newCostPred ) {
-
+    if (newCostPred < currencyRate(secondCurrency).costPred) {
       currencyRate(secondCurrency).copy(costPred = newCostPred, namePred = Some(firstCurrency))
     } else {
       currencyRate(secondCurrency)
     }
-
   }
 
-  def bellmanFord(): List[String] = {
-    val updatedCurrencyRate = (0 until currencyRate.size - 1).foldLeft(currencyRate) { (_, _) =>
-      currencyRate.keys.foldLeft(currencyRate) { (acc, firstCurrency) =>
-        currencyRate.keys.foldLeft(acc) { (innerAcc, secondCurrency) =>
-          innerAcc(secondCurrency) = release(firstCurrency, secondCurrency)
-          println(currencyRate(secondCurrency))
-          println(" ")
-          innerAcc
-
-        }
+  private def findArbitragePath(startCurrency: String, currentCurrency: String, visited: Set[String], path: List[String], wallet: Float): Option[(Float, List[String])] = {
+    val updatedCurrencyRate = currencyRate.keys.foldLeft(currencyRate) { (acc, firstCurrency) =>
+      currencyRate.keys.foldLeft(acc) { (innerAcc, secondCurrency) =>
+        innerAcc(secondCurrency) = release(firstCurrency, secondCurrency)
+        innerAcc
       }
     }
 
-
-    println(currencyRate)
-    val opportunities = updatedCurrencyRate.keys.flatMap { firstCurrency =>
-      updatedCurrencyRate.keys.flatMap { secondCurrency =>
-        if (updatedCurrencyRate(secondCurrency).costPred > updatedCurrencyRate(firstCurrency).costPred + ratesMap(s"$firstCurrency-$secondCurrency")) {
-          Some(s"$firstCurrency-$secondCurrency")
-        } else {
-          None
-        }
+    val opportunities = updatedCurrencyRate.keys.flatMap { nextCurrency =>
+      if (nextCurrency == startCurrency && visited.size == currencyRate.size) {
+        val finalPath = path :+ s"$currentCurrency-$nextCurrency"
+        val buyingPrice = wallet * ratesMap(finalPath.head)
+        val sellingPrice = buyingPrice * ratesMap(finalPath.last)
+        val profit = sellingPrice - buyingPrice
+        Some((profit, finalPath))
+      } else if (!visited.contains(nextCurrency)) {
+        val newPath = path :+ s"$currentCurrency-$nextCurrency"
+        findArbitragePath(startCurrency, nextCurrency, visited + nextCurrency, newPath, wallet)
+      } else {
+        None
       }
-    }.toList
+    }
 
-    opportunities
+    opportunities.reduceOption((a, b) => if (a._1 > b._1) a else b)
   }
-
 
   def main(args: Array[String]): Unit = {
-    var previousOpportunities: List[String] = List.empty
+    loadData()
+    val startCurrency = "BTC"
+    val wallet: Float = 100.0f
+    val result = findArbitragePath(startCurrency, startCurrency, Set(startCurrency), List.empty, wallet)
 
-    for (_ <- 0 until 3) {
-      Thread.sleep(1000)
-      loadData()
-      println(currencyRate)
-      println(ratesMap)
-      previousOpportunities = bellmanFord()
-      println(previousOpportunities)
+    result match {
+      case Some((profit, path)) =>
+        println("Arbitration opportunity found !")
+        println("Path : "+ path.mkString(" -> "))
+        println("Gain : "+ profit)
+      case None =>
+        println("No arbitration opportunity found.")
     }
   }
-
 }
